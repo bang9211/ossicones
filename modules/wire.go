fc9ae0deff166b6aeeade1395b35332fba452e91
bang9211 <bang9211@uangel.com>
2021-08-12 11:13:44 +0900

    ADDED wire DI

---
+++ modules/wire.go
@@ -0,0 +1,21 @@
+//+build wireinject
+
+package modules
+
+import (
+	"github.com/bang9211/ossicones/implements/ossiconesblockchain"
+	"github.com/bang9211/ossicones/interfaces/blockchain"
+	"github.com/google/wire"
+)
+
+var MySet = wire.NewSet(
+	wire.InterfaceValue(new(blockchain.Blockchain),
+		ossiconesblockchain.ObtainBlockchain()),
+)
+
+// var MySet = wire.NewSet(wire.InterfaceValue(new(io.Reader), os.Stdin))
+
+func initBlockchain() (blockchain.Blockchain, error) {
+	wire.Build(MySet)
+	return nil, nil
+}
