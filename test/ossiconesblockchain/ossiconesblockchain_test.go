8c704a89a6409f565b9cbd238b91719c62764c0f
bang9211 <gksdudxkr@gmail.com>
2021-08-28 23:58:53 +0900

    Add test init

--- test/ossiconesblockchain/ossiconesblockchain_test.go
+++ test/ossiconesblockchain/ossiconesblockchain_test.go
@@ -1,7 +1,48 @@
 package ossiconesblockchain
 
-import "testing"
+import (
+	"fmt"
+	"log"
+	"testing"
 
-func TestOssiconesBlockchain(t *testing.T) {
+	"github.com/bang9211/ossicones/modules"
+	"github.com/bang9211/ossicones/utils"
+)
+
+func init() {
+	isRunning, err := utils.IsRunning()
+	if err != nil {
+		fmt.Printf("Failed to check the process is already running")
+		log.Fatal(err)
+	} else if isRunning {
+		log.Fatal("The process is already running")
+	}
+
+	homePath, err := utils.GetOrSetHomePath()
+	if err != nil {
+		fmt.Printf("Failed to GetOrSetHomePath")
+		log.Fatal(err)
+	}
+	// Dependency Injection using Wire
+	modules.InitModules(homePath)
+}
+
+func close() {
+	modules.Close()
+}
+
+func TestAddBlock(t *testing.T) {
+
+}
+
+func TestAllBlocks(t *testing.T) {
+
+}
+
+func TestPrintBlock(t *testing.T) {
+
+}
+
+func TestGetBlock(t *testing.T) {
 
 }
