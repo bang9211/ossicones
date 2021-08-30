765822dd9d871c8f9c3c9f7f13826a1133141d72
bang9211 <bang9211@uangel.com>
2021-08-30 12:49:57 +0900

    WRITING ossiconesblockchain_test.go

--- go.mod
+++ go.mod
@@ -4,15 +4,16 @@ go 1.17
 
 require (
 	github.com/google/wire v0.5.0
+	github.com/gorilla/mux v1.8.0
 	github.com/mitchellh/go-ps v1.0.0
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.8.1
+	github.com/stretchr/testify v1.7.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
-	github.com/google/subcommands v1.0.1 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
@@ -22,11 +23,9 @@ require (
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
-	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 	golang.org/x/text v0.3.5 // indirect
-	golang.org/x/tools v0.1.2 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/ini.v1 v1.62.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- test/ossiconesblockchain/ossiconesblockchain_test.go
+++ test/ossiconesblockchain/ossiconesblockchain_test.go
@@ -1,38 +1,51 @@
 package ossiconesblockchain
 
 import (
-	"fmt"
-	"log"
 	"testing"
 
+	"github.com/bang9211/ossicones/interfaces/blockchain"
 	"github.com/bang9211/ossicones/modules"
-	"github.com/bang9211/ossicones/utils"
+
+	. "github.com/stretchr/testify/assert"
 )
 
-func init() {
-	isRunning, err := utils.IsRunning()
-	if err != nil {
-		fmt.Printf("Failed to check the process is already running")
-		log.Fatal(err)
-	} else if isRunning {
-		log.Fatal("The process is already running")
-	}
-
-	homePath, err := utils.GetOrSetHomePath()
-	if err != nil {
-		fmt.Printf("Failed to GetOrSetHomePath")
-		log.Fatal(err)
-	}
-	// Dependency Injection using Wire
-	modules.InitModules(homePath)
-}
-
-func close() {
-	modules.Close()
+var addblocktests = []struct {
+	title string
+	in    string
+	out   int
+}{
+	{"Added 1 block", "Test Data1", 2},
+	{"Added 2 block", "Test Data2", 3},
+	{"Added 3 block", "Test Data3", 4},
+	{"Added 4 block", "Test Data4", 5},
 }
 
 func TestAddBlock(t *testing.T) {
+	config, err := modules.InitConfig()
+	Nil(t, err)
 
+	bc, err := modules.InitBlockchain(config)
+	Nil(t, err)
+
+	blocks := bc.AllBlocks()
+	Equal(t, len(blocks), 1)
+
+	block, success := blocks[0].(blockchain.Block)
+	True(t, success)
+	Equal(t, "Genesis OssiconesBlock", block.GetData())
+
+	for i, test := range addblocktests {
+		t.Run(test.title, func(t *testing.T) {
+			bc.AddBlock(test.in)
+
+			blocks = bc.AllBlocks()
+			Equal(t, len(blocks), test.out)
+
+			block, success := blocks[i+1].(blockchain.Block)
+			True(t, success)
+			Equal(t, test.in, block.GetData())
+		})
+	}
 }
 
 func TestAllBlocks(t *testing.T) {
