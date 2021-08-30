2b651e8e83514481b1ecf52ec2d6585faba92351
bang9211 <bang9211@uangel.com>
2021-08-30 13:44:29 +0900

    WRITING ossiconesblockchain_test.go

--- test/ossiconesblockchain/ossiconesblockchain_test.go
+++ test/ossiconesblockchain/ossiconesblockchain_test.go
@@ -20,6 +20,17 @@ var addblocktests = []struct {
 	{"Added 4 block", "Test Data4", 5},
 }
 
+var allblockstests = []struct {
+	title string
+	in    string
+	out   int
+}{
+	{"Added 1 block", "Test Data1", 2},
+	{"Added 2 block", "Test Data2", 3},
+	{"Added 3 block", "Test Data3", 4},
+	{"Added 4 block", "Test Data4", 5},
+}
+
 func TestAddBlock(t *testing.T) {
 	config, err := modules.InitConfig()
 	Nil(t, err)
@@ -49,7 +60,31 @@ func TestAddBlock(t *testing.T) {
 }
 
 func TestAllBlocks(t *testing.T) {
+	config, err := modules.InitConfig()
+	Nil(t, err)
 
+	bc, err := modules.InitBlockchain(config)
+	Nil(t, err)
+
+	blocks := bc.AllBlocks()
+	Equal(t, len(blocks), 1)
+
+	block, success := blocks[0].(blockchain.Block)
+	True(t, success)
+	Equal(t, "Genesis OssiconesBlock", block.GetData())
+
+	for i, test := range addblocktests {
+		t.Run(test.title, func(t *testing.T) {
+			bc.AddBlock(test.in)
+
+			blocks = bc.AllBlocks()
+			Equal(t, len(blocks), test.out)
+
+			block, success := blocks[i+1].(blockchain.Block)
+			True(t, success)
+			Equal(t, test.in, block.GetData())
+		})
+	}
 }
 
 func TestPrintBlock(t *testing.T) {
