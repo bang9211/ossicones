71d60b9985c8f8d2c66f71c10dfdcc956d6e95a9
bang9211 <gksdudxkr@gmail.com>
2021-10-19 20:19:19 +0900

    Removed once, singleton pattern

--- implements/bolt/bolt.go
+++ implements/bolt/bolt.go
@@ -2,11 +2,9 @@ package bolt
 
 import (
 	"log"
-	"sync"
 
 	"github.com/bang9211/ossicones/interfaces/config"
 	"github.com/bang9211/ossicones/interfaces/database"
-	"github.com/bang9211/ossicones/utils"
 	"github.com/boltdb/bolt"
 )
 
@@ -23,21 +21,14 @@ type BoltDB struct {
 	bolt   *bolt.DB
 }
 
-var bt *BoltDB
-var once sync.Once
-
-func GetOrCreate(config config.Config) database.Database {
-	if bt == nil {
-		once.Do(func() {
-			bt = &BoltDB{
-				config: config,
-			}
-		})
-		err := bt.init()
-		if err != nil {
-			bt = nil
-			return nil
-		}
+func New(config config.Config) database.Database {
+	bt := &BoltDB{
+		config: config,
+	}
+	err := bt.init()
+	if err != nil {
+		log.Printf("failed to init BoltDB : %s", err)
+		return nil
 	}
 
 	return bt
@@ -50,9 +41,11 @@ func (bdb *BoltDB) init() error {
 	if err != nil {
 		return err
 	}
-	err = bt.bolt.Update(func(t *bolt.Tx) error {
+	err = bdb.bolt.Update(func(t *bolt.Tx) error {
 		_, err := t.CreateBucketIfNotExists([]byte(dataBucket))
-		utils.HandleError(err)
+		if err != nil {
+			return err
+		}
 		_, err = t.CreateBucketIfNotExists([]byte(blocksBucket))
 		return err
 	})
@@ -65,7 +58,7 @@ func (bdb *BoltDB) init() error {
 
 func (bdb *BoltDB) SaveBlock(hash string, data []byte) error {
 	log.Printf("Saving Block %s\nData: %b", hash, data)
-	return bt.bolt.Update(func(t *bolt.Tx) error {
+	return bdb.bolt.Update(func(t *bolt.Tx) error {
 		bucket := t.Bucket([]byte(blocksBucket))
 		err := bucket.Put([]byte(hash), data)
 		return err
@@ -74,7 +67,7 @@ func (bdb *BoltDB) SaveBlock(hash string, data []byte) error {
 
 func (bdb *BoltDB) SaveBlockchain(data []byte) error {
 	log.Printf("Saving Blockchain : %b", data)
-	return bt.bolt.Update(func(t *bolt.Tx) error {
+	return bdb.bolt.Update(func(t *bolt.Tx) error {
 		bucket := t.Bucket([]byte(dataBucket))
 		err := bucket.Put([]byte(checkpoint), data)
 		return err
@@ -83,7 +76,7 @@ func (bdb *BoltDB) SaveBlockchain(data []byte) error {
 
 func (bdb *BoltDB) GetCheckpoint() ([]byte, error) {
 	var data []byte
-	err := bt.bolt.View(func(t *bolt.Tx) error {
+	err := bdb.bolt.View(func(t *bolt.Tx) error {
 		bucket := t.Bucket([]byte(dataBucket))
 		data = bucket.Get([]byte(checkpoint))
 		return nil
@@ -96,7 +89,7 @@ func (bdb *BoltDB) GetCheckpoint() ([]byte, error) {
 
 func (bdb *BoltDB) GetBlock(hash string) ([]byte, error) {
 	var data []byte
-	err := bt.bolt.View(func(t *bolt.Tx) error {
+	err := bdb.bolt.View(func(t *bolt.Tx) error {
 		bucket := t.Bucket([]byte(blocksBucket))
 		data = bucket.Get([]byte(hash))
 		return nil
@@ -108,5 +101,5 @@ func (bdb *BoltDB) GetBlock(hash string) ([]byte, error) {
 }
 
 func (bdb *BoltDB) Close() error {
-	return bdb.Close()
+	return bdb.bolt.Close()
 }
