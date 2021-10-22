289641b1037703265353a12e6b77a6c0f8566189
bang9211 <gksdudxkr@gmail.com>
2021-10-22 20:19:19 +0900

    Modified testUtils

--- tests/ossiconesblockchain/testUtils.go
+++ tests/ossiconesblockchain/testUtils.go
@@ -1,32 +1,40 @@
 package ossiconesblockchain
 
 import (
+	"fmt"
+
+	"github.com/bang9211/ossicones/implements/ossiconesblockchain"
 	"github.com/bang9211/ossicones/interfaces/blockchain"
 	"github.com/bang9211/ossicones/interfaces/config"
+	"github.com/bang9211/ossicones/interfaces/database"
 	"github.com/bang9211/ossicones/modules"
 	wirejacket "github.com/bang9211/wire-jacket"
 )
 
 const genesisBlockData = "TEST_GENESIS_BLOCK_DATA"
 
-func initTest() (config.Config, blockchain.Blockchain, error) {
+func initTest() (config.Config, database.Database, blockchain.Blockchain, error) {
 	cfg := wirejacket.GetConfig()
 
-	bc, err := modules.InjectOssiconesBlockchain(cfg)
+	db, err := modules.InjectBolt(cfg)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
 
-	err = bc.Reset()
-	if err != nil {
-		return nil, nil, err
+	bc := ossiconesblockchain.New(cfg, db)
+	if bc == nil {
+		return nil, nil, nil, fmt.Errorf("failed to New()")
 	}
 
-	return cfg, bc, nil
+	return cfg, db, bc, nil
 }
 
-func closeTest(cfg config.Config, bc blockchain.Blockchain) error {
+func closeTest(cfg config.Config, db database.Database, bc blockchain.Blockchain) error {
 	err := bc.Close()
+	if err != nil {
+		return err
+	}
+	err = db.Close()
 	if err != nil {
 		return err
 	}
