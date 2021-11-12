0bb00da3ed315ea1458046e6f3730111cadda2db
bang9211 <bang9211@uangel.com>
2021-11-12 16:58:52 +0900

    Adding defaultexplorerserver test

--- implements/defaultexplorerserver/defaultexplorerserver_test.go
+++ implements/defaultexplorerserver/defaultexplorerserver_test.go
@@ -5,32 +5,64 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/bang9211/ossicones/implements/defaultexplorerserver"
+	"github.com/bang9211/ossicones/implements/bolt"
+	"github.com/bang9211/ossicones/implements/ossiconesblockchain"
+	"github.com/bang9211/ossicones/interfaces/blockchain"
+	"github.com/bang9211/ossicones/interfaces/config"
 	"github.com/bang9211/ossicones/interfaces/explorerserver"
+	"github.com/bang9211/ossicones/utils"
+	wirejacket "github.com/bang9211/wire-jacket"
 
-	. "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
+const genesisBlockData = "TEST_GENESIS_BLOCK_DATA"
+
 func TestImplementExplorerServer(t *testing.T) {
-	Implements(t, (*explorerserver.ExplorerServer)(nil), new(defaultexplorerserver.DefaultExplorerServer),
-		"It must implements of interface explorerserver.ExplorerServer")
+	assert.Implements(t, (*explorerserver.ExplorerServer)(nil), new(DefaultExplorerServer),
+		"It must assert.Implements of interface explorerserver.ExplorerServer")
 }
 
 func TestServe(t *testing.T) {
+	_, err := utils.GetOrSetHomePath()
+	assert.NoError(t, err)
 	t.Setenv("OSSICONES_BLOCKCHAIN_GENESIS_BLOCK_DATA", genesisBlockData)
 	cfg, bc, es, err := initTest()
-	NoError(t, err, "Failed to initTest()")
+	assert.NoError(t, err, "Failed to initTest()")
 	defer closeTest(cfg, bc, es)
 
-	des, ok := es.(*defaultexplorerserver.DefaultExplorerServer)
-	True(t, ok)
+	des, ok := es.(*DefaultExplorerServer)
+	assert.True(t, ok)
 
 	request := httptest.NewRequest(http.MethodGet, "/", nil)
 	responseRecorder := httptest.NewRecorder()
 	des.home(responseRecorder, request)
+	assert.Equal(t, responseRecorder.Code, 200)
 }
 
 func TestClose(t *testing.T) {
-	Implements(t, (*explorerserver.ExplorerServer)(nil), new(defaultexplorerserver.DefaultExplorerServer),
-		"It must implements of interface explorerserver.ExplorerServer")
+	assert.Implements(t, (*explorerserver.ExplorerServer)(nil), new(DefaultExplorerServer),
+		"It must assert.Implements of interface explorerserver.ExplorerServer")
+}
+
+func initTest() (config.Config, blockchain.Blockchain, explorerserver.ExplorerServer, error) {
+	cfg := wirejacket.GetConfig()
+
+	db := bolt.New(cfg)
+	bc := ossiconesblockchain.New(cfg, db)
+	es := New(cfg, bc)
+
+	return cfg, bc, es, nil
+}
+
+func closeTest(cfg config.Config, bc blockchain.Blockchain, es explorerserver.ExplorerServer) error {
+	err := es.Close()
+	if err != nil {
+		return err
+	}
+	err = bc.Close()
+	if err != nil {
+		return err
+	}
+	return cfg.Close()
 }
